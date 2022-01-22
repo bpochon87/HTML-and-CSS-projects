6fe8075b336de90260800ac24fc01b3c999db6ef
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-01-22 14:04:35 -0700

    Added Basic_HTML_and_JavaScript_1 folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Welcome to the beep boop beep world.");
+
+function My_First_Function()
+	{
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innterHTML = 
+	result;
+	}
